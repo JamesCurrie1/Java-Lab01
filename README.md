6e0deef4ccec0d9607a23d54215244a041b7c67a
JamesCurrie1 <90404815+JamesCurrie1@users.noreply.github.com>
2021-09-16 14:46:21 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Java-Lab01
+ Lab01
---
+++ src/main/java/Battle.java
@@ -0,0 +1,20 @@
+public class Battle extends Lab01 {
+    public Battle(String chikapuName, float chikapuAttack, float chikapuDefense, float chikapuHP
+    , String zarichardName, float zarichardAttack, float zarichardDefense, float zarichardHP){
+
+    }
+
+
+    public boolean isMonster1Dead(){
+        return true;
+    }
+    public boolean isMonster2Dead(){
+        return true;
+    }
+    public void simulateRound(){
+
+
+    }
+
+
+}
